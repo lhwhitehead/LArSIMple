13d27b765e3eceb1d0e3db3e61d32c4f33510c57
Leigh Whitehead <leigh.howard.whitehead@cern.ch>
2022-02-25 06:39:33 -0600

    Covenient to have genie set up too

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ setup geant4 v4_10_6_p01d -f Linux64bit+3.10-2.17 -q e20:prof
 setup root v6_22_08d -f Linux64bit+3.10-2.17 -q e20:p392:prof
 setup clhep v2_4_4_1 -q e20:prof
 setup cmake v3_9_2
+setup genie v3_00_06 -q e20:prof
