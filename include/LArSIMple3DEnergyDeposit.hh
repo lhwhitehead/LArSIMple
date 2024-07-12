282fbde3f67ff119d844828a5b171287845486a3
Leigh Whitehead <leigh.h.whitehead@gmail.com>
2024-07-12 14:35:03 +0100

    First pandora-style file

--- include/LArSIMple3DEnergyDeposit.hh
+++ include/LArSIMple3DEnergyDeposit.hh
@@ -1,119 +1,288 @@
+/**
+ *  @file   PandoraSDK/include/Objects/CartesianVector.h
+ * 
+ *  @brief  Header file for the cartesian vector class.
+ * 
+ *  $Log: $
+ */
+
 #ifndef LArSIMple3DEnergyDeposit_h
 #define LArSIMple3DEnergyDeposit_h
 
 #include "G4ThreeVector.hh"
 #include <vector>
 
+/**
+ *  @brief  CartesianVector class
+ */
 class LArSIMple3DEnergyDeposit
 {
 public:
+    /**
+     *  @brief  Default constructor
+     */
     LArSIMple3DEnergyDeposit();
+
+    /**
+     *  @brief  Copy constructor
+     *
+     *  @param  rhs LArSIMple3DEnergyDeposit object to copy
+     */
     LArSIMple3DEnergyDeposit(const LArSIMple3DEnergyDeposit &rhs);
+
+    /**
+     *  @brief  Destructor
+     */
     ~LArSIMple3DEnergyDeposit();
 
+    /**
+     *  @brief  Get the position vector of the energy deposit
+     *
+     *  @return The xyz position of the energy deposit
+     */
     G4ThreeVector GetPosition() const;
-    G4ThreeVector GetUVWPosition() const;
-    double GetTime() const
-    {
-        return fTime;
-    };
-    double GetEnergy() const
-    {
-        return fEnergy;
-    };
-    double GetDeDx() const
-    {
-        return fDeDx;
-    };
-    int GetParticlePDG() const
-    {
-        return fParticlePDG;
-    };
-    int GetParticleTrackID() const
-    {
-        return fParticleTrackID;
-    };
-    int GetParticleProcess() const
-    {
-        return fParticleProcess;
-    };
 
+    /**
+     *  @brief  Get the position vector of the energy deposit in U, V and W readout planes
+     *
+     *  @return The UVW position of the energy deposit
+     */
+    G4ThreeVector GetUVWPosition() const;
+
+    /**
+     *  @brief  Get the time of the energy deposit
+     *
+     *  @return The time of the energy deposit
+     */
+    double GetTime() const;
+
+    /**
+     *  @brief  Get the deposited energy
+     *
+     *  @return The deposited energy
+     */
+    double GetEnergy() const;
+
+    /**
+     *  @brief  Get the deposited energy per unit length
+     *
+     *  @return The dE/dx
+     */
+    double GetDeDx() const;
+
+    /**
+     *  @brief  Get the true particle code according to the PDG
+     *
+     *  @return The PDG code
+     */
+    int GetParticlePDG() const;
+
+    /**
+     *  @brief  Get the Geant4 id of the true particle that deposited the energy
+     *
+     *  @return The Geant4 track id
+     */
+    int GetParticleTrackID() const;
+
+    /**
+     *  @brief  Get the process that caused the energy deposition
+     *
+     *  @return The process as an integer
+     */
+    int GetParticleProcess() const;
+
+    /**
+     *  @brief  Set position and time using (x,y,z,t)
+     *
+     *  @param  x the x position coordinate
+     *  @param  y the y position coordinate
+     *  @param  z the z position coordinate
+     *  @param  t the time of the energy deposit
+     */
     void SetPositionAndTime(const double x, const double y, const double z, const double time);
+
+    /**
+     *  @brief  Set position and time using (x,y,z,t)
+     *
+     *  @param  pos the 3D position
+     *  @param  t the time of the energy deposit
+     */
     void SetPositionAndTime(const G4ThreeVector &pos, const double time);
+
+    /**
+     *  @brief  Calculate the U, V and W positions of the energy deposit
+     *
+     *  @param  wireAngleU the angle of the U plane to the z axis
+     *  @param  wireAngleV the angle of the V plane to the z axis
+     *  @param  wireAngleW the angle of the W plane to the z axis
+     */
     void CalculateUVW(const double wireAngleU, const double wireAngleV, const double wireAngleW);
+
+    /**
+     *  @brief  Set the energy of the deposit
+     *
+     *  @param  energy the deposited energy
+     */
     void SetEnergy(const double energy);
+
+    /**
+     *  @brief  Set the energy per unit length of the deposit
+     *
+     *  @param  dedx the dE/dx
+     */
     void SetDeDx(const double dedx);
+
+    /**
+     *  @brief  Set the true particle information
+     *
+     *  @param  energy the deposited energy
+     */
     void SetParticleInfo(const int pdg, const int trackID, const int processCode);
 
-    void AddFeature(const float val)
-    {
-        fFeatures.emplace_back(val);
-    };
-    std::vector<float> GetFeatures() const
-    {
-        return fFeatures;
-    };
+    /**
+     *  @brief  Add a feature to the energy deposit, such as number of neighbours
+     *
+     *  @param  val the value of the feature to add
+     */
+    void AddFeature(const float val);
 
+    /**
+     *  @brief  Get the vector of features associated to the energy deposit
+     *
+     *  @return The vector of feature values
+     */
+    std::vector<float> GetFeatures() const;
+
+    /**
+     *  @brief  Get the flat vector representation of the energy deposit
+     *
+     *  @return The vector of position, time, energy, dE/dx, features and truth information values
+     */
     std::vector<float> GetFlatRepresentation() const;
 
-    unsigned int GetNCoordinates() const
-    {
-        return 3;
-    }; // Hardcoded for now
-    unsigned int GetNFeatures() const
-    {
-        return fFeatures.size();
-    };
-    unsigned int GetNTruths() const
-    {
-        return 2;
-    }; // Hardcoded for now
+    /**
+     *  @brief  Get the number of position coordinates
+     *
+     *  @return The number of position coordinates
+     */
+    unsigned int GetNCoordinates() const;
+
+    /**
+     *  @brief  Get the number of wire position coordinates
+     *
+     *  @return The number of wire position coordinates
+     */
+    unsigned int GetNWireCoordinates() const;
+
+    /**
+     *  @brief  Get the number of features
+     *
+     *  @return The number of features
+     */
+    unsigned int GetNFeatures() const;
+
+    /**
+     *  @brief  Get the number of bits of truth information
+     *
+     *  @return The number of truth information values
+     */
+    unsigned int GetNTruths() const;
+
+    /**
+     *  @brief  Print a summary of the class to the screen
+     */
     void PrintSummary() const;
 
 private:
-    double fPosX;
-    double fPosY;
-    double fPosZ;
-    double fTime;
+    G4ThreeVector fPosition;      ///< 3D position of the energy deposit
+    double fTime;                 ///< Time of the energy deposit
 
-    // Wire planes
-    double fPosU;
-    double fPosV;
-    double fPosW;
+    double fPosU;                 ///< Position in the U wire plane projection 
+    double fPosV;                 ///< Position in the V wire plane projection
+    double fPosW;                 ///< Position in the W wire plane projection 
 
-    double fEnergy;
-    double fDeDx;
+    double fEnergy;               ///< The energy deposited
+    double fDeDx;                 ///< The dE/dx
 
-    // Information about the particle that created the energy
-    int fParticlePDG;
-    int fParticleTrackID;
-    int fParticleProcess;
+    int fParticlePDG;             ///< The true particle type that produced the energy deposit
+    int fParticleTrackID;         ///< The Geant4 track id of the particle that produced the energy deposit
+    int fParticleProcess;         ///< The process by which the energy was deposited
 
-    std::vector<float> fFeatures;
+    std::vector<float> fFeatures; ///< The vector of the associated features
 };
+
+//------------------------------------------------------------------------------------------------------------------------------------------
 
 inline G4ThreeVector LArSIMple3DEnergyDeposit::GetPosition() const
 {
-    return G4ThreeVector(fPosX, fPosY, fPosZ);
+    return fPosition;
 }
+
+//------------------------------------------------------------------------------------------------------------------------------------------
 
 inline G4ThreeVector LArSIMple3DEnergyDeposit::GetUVWPosition() const
 {
     return G4ThreeVector(fPosU, fPosV, fPosW);
 }
 
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline double LArSIMple3DEnergyDeposit::GetTime() const
+{
+    return fTime;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline double LArSIMple3DEnergyDeposit::GetEnergy() const
+{
+    return fEnergy;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline double LArSIMple3DEnergyDeposit::GetDeDx() const
+{
+    return fDeDx;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline int LArSIMple3DEnergyDeposit::GetParticlePDG() const
+{
+    return fParticlePDG;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline int LArSIMple3DEnergyDeposit::GetParticleTrackID() const
+{
+    return fParticleTrackID;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline int LArSIMple3DEnergyDeposit::GetParticleProcess() const
+{
+    return fParticleProcess;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
 inline void LArSIMple3DEnergyDeposit::SetPositionAndTime(const double x, const double y, const double z, const double time)
 {
-    fPosX = x;
-    fPosY = y;
-    fPosZ = z;
-    fTime = time;
+    return this->SetPositionAndTime(G4ThreeVector(x, y, z), time);
 }
+
+//------------------------------------------------------------------------------------------------------------------------------------------
 
 inline void LArSIMple3DEnergyDeposit::SetPositionAndTime(const G4ThreeVector &pos, const double time)
 {
-    this->SetPositionAndTime(pos.x(), pos.y(), pos.z(), time);
+    fPosition = pos;
+    fTime = time;
 }
+
+//------------------------------------------------------------------------------------------------------------------------------------------
 
 inline void LArSIMple3DEnergyDeposit::SetEnergy(const double energy)
 {
@@ -122,11 +291,15 @@ inline void LArSIMple3DEnergyDeposit::SetEnergy(const double energy)
     fFeatures.emplace_back(fEnergy);
 }
 
+//------------------------------------------------------------------------------------------------------------------------------------------
+
 inline void LArSIMple3DEnergyDeposit::SetDeDx(const double dedx)
 {
     fDeDx = dedx;
     fFeatures.emplace_back(fDeDx);
 }
+
+//------------------------------------------------------------------------------------------------------------------------------------------
 
 inline void LArSIMple3DEnergyDeposit::SetParticleInfo(const int pdg, const int trackID, const int processCode)
 {
@@ -134,6 +307,50 @@ inline void LArSIMple3DEnergyDeposit::SetParticleInfo(const int pdg, const int t
     fParticleTrackID = trackID;
     fParticleProcess = processCode;
 }
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline void LArSIMple3DEnergyDeposit::AddFeature(const float val)
+{
+    fFeatures.emplace_back(val);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline std::vector<float> LArSIMple3DEnergyDeposit::GetFeatures() const
+{
+    return fFeatures;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline unsigned int LArSIMple3DEnergyDeposit::GetNCoordinates() const
+{
+    return 3;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline unsigned int LArSIMple3DEnergyDeposit::GetNWireCoordinates() const
+{
+    return 3;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline unsigned int LArSIMple3DEnergyDeposit::GetNFeatures() const
+{
+    return fFeatures.size();
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline unsigned int LArSIMple3DEnergyDeposit::GetNTruths() const
+{
+    return 3;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
 
 inline std::vector<float> LArSIMple3DEnergyDeposit::GetFlatRepresentation() const
 {
