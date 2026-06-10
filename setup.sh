875da5ea0d72ea8983e4ee23828281c1d0bd9355
Leigh Whitehead <leigh.h.whitehead@gmail.com>
2026-06-10 02:11:09 -0500

    Tidy up the setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,7 @@
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
-#setup geant4 v4_10_6_p01d -f Linux64bit+3.10-2.17 -q e20:prof
-#setup root v6_22_08d -f Linux64bit+3.10-2.17 -q e20:p392:prof
-#setup clhep v2_4_4_1 -q e20:prof
-#setup cmake v3_9_2
-#setup genie v3_00_06n -q e20:prof
-
 setup gcc v12_1_0
 setup geant4 v4_11_1_p01ba -f Linux64bit+3.10-2.17 -q e26:prof
 setup root v6_28_12 -f Linux64bit+3.10-2.17 -q e26:p3915:prof
 setup clhep v2_4_5_3a -q e26:prof
 setup cmake v3_27_4
-#setup larpandoracontent v04_09_00 -q e26:prof
 setup larpandoracontent v05_00_00 -q e26:prof
