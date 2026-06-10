57959e4024bbbfe65a26cceb54de458544e7039f
Leigh Whitehead <leigh.h.whitehead@gmail.com>
2026-06-10 03:45:19 -0500

    Remember to update cmake with new Pandora Messenger!

--- cmake/PandoraSources.cmake
+++ cmake/PandoraSources.cmake
@@ -1,9 +1,11 @@
 list(APPEND LARSIMPLE_SOURCES
     src/LArSIMplePandoraContent.cc
+    src/LArSIMplePandoraMessenger.cc
     src/LArSIMplePandoraWriter.cc
 )
 
 list(APPEND LARSIMPLE_HEADERS
     include/LArSIMplePandoraContent.hh
+    include/LArSIMplePandoraMessenger.hh
     include/LArSIMplePandoraWriter.hh
 )
